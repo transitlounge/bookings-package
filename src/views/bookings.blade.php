2f9f2515baf566734892868c13a7887bbfc925dd
Transitlounge <info@transitlounge.one>
2024-03-16 19:17:06 +0800

    changed the index page

--- src/views/bookings.blade.php
+++ src/views/bookings.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Booking Page</title>
-</head>
-<body>
-    <h1>Hello, World from the Booking Package!</h1>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Booking Package</title>
+
+    </head>
+    <body class="font-sans antialiased">
+      <h1>Hello, World from the Booking Package!</h1>
+    </body>
 </html>
