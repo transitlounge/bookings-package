2ceb2fcc3d2d552f5180f348eb66d11003f1ab41
Transitlounge <info@transitlounge.one>
2024-03-16 18:58:24 +0800

    fix path

--- src/BookingsServiceProvider.php
+++ src/BookingsServiceProvider.php
@@ -10,7 +10,7 @@ class BookingsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/src/views', 'transitlounge');
+        $this->loadViewsFrom(__DIR__.'/views', 'transitlounge');
     }
 
     public function register()
